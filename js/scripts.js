7c5473486afcb73710b7d578e73b0d167161f5a8
Conor Smith <consmith18@gmail.com>
2019-11-09 23:30:23 +0000

    Adding post js to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -259,7 +259,7 @@ function initBBSRMap() {
 
 // alert_markup
 function alert_markup(alert_type, msg) {
-    return '<div class="alert alert-' + alert_type + '" role="alert">' + msg + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span>&times;</span></button></div>';
+    return '<div class="alert alert-' + 'info' + '" role="alert">' + msg + '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span>&times;</span></button></div>';
 }
 
 // MD5 Encoding
